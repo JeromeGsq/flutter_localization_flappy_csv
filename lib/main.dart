5ded89911817ec8d6504cbbc7b95645a025174a7
Jerome Ghesquiere <jerome.ghesquiere@orange.com>
2020-02-04 10:43:06 +0100

    Update package & add langage and region

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import 'i18n/i18n.dart';
 
 void main() => runApp(MyApp());
 
-// Flappy doesn't support country. meh
 final supportedLocales = [
-  const Locale('en', ''),
-  const Locale('es', ''),
-  const Locale('fr', ''),
+  const Locale('en', 'GB'),
+  const Locale('en', 'US'),
+  const Locale('es', 'ES'),
+  const Locale('fr', 'FR'),
+  const Locale('de'),
 ];
 
 class MyApp extends StatelessWidget {
@@ -43,9 +44,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // Swap locale at runtime
   int localIndex = 0;
+
   void _swapLanguage() {
     setState(() {
-      localIndex = localIndex >= supportedLocales.length - 1 ? 0 : localIndex + 1;
+      localIndex =
+          localIndex >= supportedLocales.length - 1 ? 0 : localIndex + 1;
       I18n.load(supportedLocales[localIndex]);
     });
   }
@@ -62,6 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(I18n.of(context).title),
             Text(I18n.of(context).hello),
+            Text(I18n.of(context).description(name: "Dash")),
           ],
         ),
       ),
